afc58f44bb0ce74b5239cbe4cbfc1a985f3c9a37
korvictech <korvictech@gmail.com>
2018-10-23 02:12:20 -0700

    Adding Assets registration to the platform

---
+++ controller/asset.js
@@ -0,0 +1,43 @@
+const hyperConfig = require('../config/hyperconfig');
+//const cardService = require('../services/cardService');
+const BusinessNetworkConnection = require('composer-client').BusinessNetworkConnection;
+let businessNetworkConnection  = new BusinessNetworkConnection();
+
+
+
+async function addRealEstate(realEstateData) {
+
+    try {
+
+        let type = "RealEstate";
+        let userType = "User";
+
+        let businessNetDefination = await businessNetworkConnection.connect(hyperConfig.networkAdminCard);
+        let factory = businessNetworkConnection.getBusinessNetwork().getFactory();
+
+        let assetRegistry = await businessNetworkConnection.getAssetRegistry(`${hyperConfig.ns}.${type}`);
+
+        let ownerRelations = factory.newRelationship(hyperConfig.ns, userType, realEstateData.owner);
+        let newRealEstate = factory.newResource(hyperConfig.ns, type, realEstateData.assetId);
+
+        delete realEstateData.owner;
+
+        newRealEstate.owner = ownerRelations;
+
+        newRealEstate = Object.assign(newRealEstate, realEstateData);
+
+        let result = await assetRegistry.add(newRealEstate);
+
+        res.json({success: true, msg: 'Your real estate registered'});
+    } catch (err) {
+
+        errMessage = typeof err == 'string' ? err : err.message;
+        res.json({success: true, msg: "Create Real Estate failed"});
+    }
+
+
+}
+
+module.exports = {
+    addRealEstate
+}
